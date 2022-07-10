d28d6af051f6a265431067423eba345c687d433a
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-07-10 13:09:40 -0300

    Validando e salvando email

--- src/main/java/com/paulocavalcante/userLogin/registration/RegistrationService.java
+++ src/main/java/com/paulocavalcante/userLogin/registration/RegistrationService.java
@@ -1,7 +1,32 @@
 package com.paulocavalcante.userLogin.registration;
 
+import com.paulocavalcante.userLogin.AppUser.AppUser;
+import com.paulocavalcante.userLogin.AppUser.AppUserRole;
+import com.paulocavalcante.userLogin.AppUser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+       boolean isValidEmail =  emailValidator.test(request.getEmail());
+
+       if(!isValidEmail) {
+           throw new IllegalStateException("email not valid");
+       }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstname(),
+                        request.getLastname(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
--- src/main/java/com/paulocavalcante/userLogin/security/config/WebSecurityConfig.java
+++ src/main/java/com/paulocavalcante/userLogin/security/config/WebSecurityConfig.java
@@ -1,11 +1,15 @@
 package com.paulocavalcante.userLogin.security.config;
 
+import com.paulocavalcante.userLogin.AppUser.AppUserService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Configuration
@@ -13,12 +17,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private  final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http
                .csrf().disable()
                .authorizeRequests()
-               .antMatchers("api/v*/registration/**")
+               .antMatchers("/api/v*/registration/**")
                .permitAll()
                .anyRequest()
                .authenticated().and()
@@ -29,6 +36,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider();
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
     }
 }
