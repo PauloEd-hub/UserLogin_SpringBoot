3fbee8ebfecb80f8f732aed5d81c55278f85a41c
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-07-16 16:11:51 -0300

    Confirmação de token para email.

--- src/main/java/com/paulocavalcante/userLogin/registration/RegistrationService.java
+++ src/main/java/com/paulocavalcante/userLogin/registration/RegistrationService.java
@@ -3,8 +3,11 @@ package com.paulocavalcante.userLogin.registration;
 import com.paulocavalcante.userLogin.AppUser.AppUser;
 import com.paulocavalcante.userLogin.AppUser.AppUserRole;
 import com.paulocavalcante.userLogin.AppUser.AppUserService;
+import com.paulocavalcante.userLogin.registration.token.ConfirmationToken;
+import com.paulocavalcante.userLogin.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +15,7 @@ public class RegistrationService {
 
     private final AppUserService appUserService;
     private final EmailValidator emailValidator;
+    private final ConfirmationTokenService confirmationTokenService;
 
     public String register(RegistrationRequest request) {
        boolean isValidEmail =  emailValidator.test(request.getEmail());
